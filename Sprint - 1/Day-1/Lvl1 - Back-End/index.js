378884beaa5f3da1665eadcb44c3e689a69236e7
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-20 20:58:24 +0530

    Update

--- Sprint - 1/Day-1/Lvl1 - Back-End/index.js
+++ Sprint - 1/Day-1/Lvl1 - Back-End/index.js
@@ -11,27 +11,31 @@ app.get("/shayari", async (req, res) => {
   try {
     const keyword = req.query.keyword;
     const response = await axios.post(
-      "https://api.openai.com/v1/completions",
+      "https://api.openai.com/v1/chat/completions",
       {
-        prompt: `Shayari about ${keyword}`,
+        messages: [
+          {
+            role: "user",
+            content: `Shayari about ${keyword}`,
+          },
+        ],
         max_tokens: 100,
-        temperature: 0.7,
-        n: 1,
         model: "gpt-3.5-turbo",
       },
       {
         headers: {
           Authorization: `Bearer ${process.env.API_KEY}`,
+
           "Content-Type": "application/json",
         },
       }
     );
 
-    const shayari = response.data.choices[0].text.trim();
+    const shayari = response.data.choices[0].message.content;
     res.json({ shayari });
   } catch (error) {
-    console.error("Error:", error.response.data);
-    res.status(500).json(error.response.data.error);
+    console.error("Error:", error);
+    res.status(500).json({ error: "Something Went Wrong" });
   }
 });
 
