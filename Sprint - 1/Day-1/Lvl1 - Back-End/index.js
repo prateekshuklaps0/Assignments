46c975cdcbbb931a84855732b3edb5bfa8c62a65
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-20 21:55:07 +0530

    Update

--- Sprint - 1/Day-1/Lvl1 - Back-End/index.js
+++ Sprint - 1/Day-1/Lvl1 - Back-End/index.js
@@ -9,32 +9,36 @@ app.use(express.json());
 
 app.get("/shayari", async (req, res) => {
   try {
-    const keyword = req.query.keyword;
+    const { content, keyword } = req.query;
+
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
         messages: [
           {
             role: "user",
-            content: `Shayari about ${keyword}`,
+            content: `Do not give any explaination, just give a ${content} including or related to ${keyword} in hindi and english.`,
           },
         ],
-        max_tokens: 100,
+        max_tokens: 3500,
         model: "gpt-3.5-turbo",
       },
       {
         headers: {
-          Authorization: `Bearer ${process.env.API_KEY}`,
-
+          // Authorization: `Bearer ${process.env.API_KEY}`,
+          // Authorization: `Bearer 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`,
           "Content-Type": "application/json",
         },
       }
     );
 
-    const shayari = response.data.choices[0].message.content;
-    res.json({ shayari });
+    const chatresponse = response.data.choices[0].message.content;
+    res.json({ chatresponse });
   } catch (error) {
-    console.error("Error:", error);
+    console.error(
+      "Error:",
+      error.response.data ? error.response.data : "Something Went Wrong"
+    );
     res.status(500).json({ error: "Something Went Wrong" });
   }
 });
