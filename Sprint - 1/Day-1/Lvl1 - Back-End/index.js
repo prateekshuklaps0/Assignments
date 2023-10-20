8c5d5fe631fc847178541d6277f0e81103517246
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-20 20:12:25 +0530

    U

--- Sprint - 1/Day-1/Lvl1 - Back-End/index.js
+++ Sprint - 1/Day-1/Lvl1 - Back-End/index.js
@@ -1,1 +1,41 @@
-console.log("yes");
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get("/shayari", async (req, res) => {
+  try {
+    const keyword = req.query.keyword;
+    const response = await axios.post(
+      // "https://api.openai.com/v1/engines/davinci-codex/completions",
+      "https://api.openai.com/v1/completions",
+      {
+        prompt: `Shayari about ${keyword}`,
+        max_tokens: 100,
+        temperature: 0.7,
+        n: 1,
+        model: "gpt-3.5-turbo",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const shayari = response.data.choices[0].text.trim();
+    res.json({ shayari });
+  } catch (error) {
+    console.error("Error:", error.response.data);
+    res.status(500).json(error.response.data.error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
