86ae49568860765065b1f2014978e828b5a83ed2
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-23 12:27:13 +0530

    o

--- Sprint-1/Day-1/Lvl1 - Front-End/src/Redux/actionTypes.ts
+++
@@ -1,5 +0,0 @@
-export const LOADING = "LOADING";
-export const ERROR = "ERROR";
-export const SUCCESS = "SUCCESS";
-export const CHANGECURRENTTYPE = "CHANGECURRENTTYPE";
-export const CHANGECONTEXT = "CHANGECONTEXT";
