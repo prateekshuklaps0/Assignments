0249321cd7ee959cbbf8e71eddeb87774c1863de
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-23 12:29:40 +0530

    z

---
+++ Sprint-1/Day-1/Lvl2 - Front-End/src/Redux/reducer.ts
@@ -0,0 +1,60 @@
+import {
+  LOADING,
+  ERROR,
+  SUCCESS,
+  CHANGECURRENTTYPE,
+  CHANGECONTEXT,
+} from "./actionTypes";
+
+export const init = {
+  isLoading: false,
+  isError: false,
+  currentType: "Shayari",
+  contentTypes: ["Shayari", "Joke", "Story", "Quote"],
+  context: "",
+  result: "",
+};
+
+const reducer = (state = init, { type, payload }: any) => {
+  switch (type) {
+    case LOADING: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    }
+    case ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        result: payload,
+      };
+    }
+    case SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        result: payload,
+      };
+    }
+    case CHANGECURRENTTYPE: {
+      return {
+        ...state,
+        currentType: payload,
+      };
+    }
+    case CHANGECONTEXT: {
+      return {
+        ...state,
+        context: payload,
+      };
+    }
+    default: {
+      return init;
+    }
+  }
+};
+
+export default reducer;
