34726c1646ef7b7f1760b414b046478169535edc
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-23 13:30:24 +0530

    f

--- Sprint-1/Day-1/Lvl1 - Front-End/src/App.tsx
+++ Sprint-1/Day-1/Lvl1 - Front-End/src/App.tsx
@@ -1,16 +1,9 @@
 import "./App.css";
-import UpperSide from "./Components/UpperSide";
-import LowerSide from "./Components/LowerSide";
 
 import { Box } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <Box className="OuterCont">
-      <UpperSide />
-      <LowerSide />
-    </Box>
-  );
+  return <Box className="OuterCont"></Box>;
 }
 
 export default App;
