229ad86ae79abe71be9967d14a8a15c7cb0f9de4
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-25 12:54:38 +0530

    d

--- Sprint-1/Day-2/Lvl1 - Front-End/src/App.tsx
+++ Sprint-1/Day-2/Lvl1 - Front-End/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
 
 import { Box } from "@chakra-ui/react";
+import Editor from "@monaco-editor/react";
 
 function App() {
-  return <Box className="OuterCont"></Box>;
+  return (
+    <Box className="OuterCont">
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        defaultValue="// Print something"
+      />
+    </Box>
+  );
 }
 
 export default App;
--- Sprint-1/Day-2/Lvl1 - Front-End/src/Redux/test.ts
+++
@@ -1,5 +0,0 @@
-const testing: string = "";
-console.log(testing);
-
-const dh: string = "";
-console.log(dh);
