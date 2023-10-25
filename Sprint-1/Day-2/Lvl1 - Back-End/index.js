f89cd28afab568aad0aa86c26c2c8dd714440ddf
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-25 11:53:47 +0530

    f

---
+++ Sprint-1/Day-2/Lvl1 - Back-End/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+// Home Route
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ msg: "Welcome to Homepage." });
+  } catch (error) {
+    res.status(500).json({ msg: "Homepage Error", error });
+  }
+});
+
+app.get("/get", async (req, res) => {
+  try {
+    const { content, keyword } = req.query;
+
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        messages: [
+          {
+            role: "user",
+            content: `Do not give any explaination, just give a ${content} including or related to ${keyword} in hindi and english.`,
+          },
+        ],
+        model: "gpt-3.5-turbo",
+        max_tokens: 3500,
+        temperature: 1,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const chatresponse = response.data.choices[0].message.content;
+    res.json({ chatresponse });
+  } catch (error) {
+    console.error(
+      "Error:",
+      error.response.data ? error.response.data : "Something Went Wrong"
+    );
+    res.status(500).json({ error: error?.response?.data?.error?.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
