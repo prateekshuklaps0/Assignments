8f3673d83b82bb597074214e377c2809aba81f82
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-26 22:27:39 +0530

    v

---
+++ Sprint-1/Day-3/LVL2- Building a Task Management System-Advanced TypeScript Project/src/reports/taskReports.ts
@@ -0,0 +1,21 @@
+import { Task } from "../models/Task";
+
+export class TaskReport {
+  constructor(private tasks: Task[]) {}
+
+  getCompletedTaskCount() {
+    // Implement logic to count completed tasks
+  }
+
+  getPendingTaskCount() {
+    // Implement logic to count pending tasks
+  }
+
+  getTasksDueToday() {
+    // Implement logic to find tasks due today
+  }
+
+  getOverdueTasks() {
+    // Implement logic to find overdue tasks
+  }
+}
