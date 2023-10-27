097fe1ec1cc1a005d182b6c093ee476b492fddd1
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-27 17:13:38 +0530

    d

--- Sprint-1/Day-4/Lvl2-Angular To-Do List App/src/app/app.module.ts
+++ Sprint-1/Day-4/Lvl2-Angular To-Do List App/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoFormComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
---
+++ Sprint-1/Day-4/Lvl2-Angular To-Do List App/src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent {
+
+}
