21e548015593a65612eba312affde576f578267d
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-11-02 13:27:48 +0530

    z

--- Sprint-2/Day-2/Lvl1 - Angular Component with Advanced Data Binding/src/app/app.component.ts
+++ Sprint-2/Day-2/Lvl1 - Angular Component with Advanced Data Binding/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
+import { productsData } from 'src/assets/productsData';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FiltersComponent } from './filters/filters.component';
 import { ProductCardComponent } from './product-card/product-card.component';
@@ -12,6 +13,8 @@ import { ProductCardComponent } from './product-card/product-card.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  products = productsData;
+
   ngOnInit(): void {
     initFlowbite();
   }
