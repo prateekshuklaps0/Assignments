25a0a03ba8a86f9426f1511627b3ff9c0c54241d
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-11-02 12:32:04 +0530

    x

--- Sprint-2/Day-2/Lvl1 - Angular Component with Advanced Data Binding/src/app/app.component.ts
+++ Sprint-2/Day-2/Lvl1 - Angular Component with Advanced Data Binding/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
---
+++ Sprint-2/Day-2/Lvl1 - Angular Component with Advanced Data Binding/src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { LogoComponent } from '../logo/logo.component';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {}
