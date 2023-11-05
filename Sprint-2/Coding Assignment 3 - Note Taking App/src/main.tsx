f50700fc97517fed662f1ddfac7b41cd89ae58d2
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-11-05 11:26:23 +0530

    y

--- Sprint-2/Coding Assignment 3 - Note Taking App/src/main.tsx
+++ Sprint-2/Coding Assignment 3 - Note Taking App/src/main.tsx
@@ -1,11 +1,12 @@
 import "./index.css";
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
+import store from "./redux/store.ts";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
