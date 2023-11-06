27d68569d5c3c5ab9328d9f16c3056387bbf8af0
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-11-06 17:45:36 +0530

    u

--- Sprint-3/Day-1 B30 GA201 Interview 2 (Round 3)/Building a Basic Social Media Web Application Frontend using Angular with UI Emphasis/src/app/app.module.ts
+++ Sprint-3/Day-1 B30 GA201 Interview 2 (Round 3)/Building a Basic Social Media Web Application Frontend using Angular with UI Emphasis/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { ViewComponent } from './view/view.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    ViewComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule
