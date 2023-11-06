bbdfc95f1bd1e98052950c397734f14d44af3858
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-11-06 17:55:39 +0530

    e

---
+++ Sprint-3/Day-1 B30 GA201 Interview 2 (Round 3)/Basic Social Media Web App/src/app/post/post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+})
+export class PostComponent {
+  userName: string = '';
+  caption: string = '';
+}
