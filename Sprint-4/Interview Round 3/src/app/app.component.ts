efae9533227252bc5394432e7bf5c78909c69d4b
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-11-24 22:03:47 +0530

    f

--- Sprint-4/Interview Round 3/src/app/app.component.ts
+++ Sprint-4/Interview Round 3/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
+import { TaskListComponent } from './task-list/task-list.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Lvl';
-}
+export class AppComponent {}
